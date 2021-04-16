1f9cef1d719d47253a58538328e72f9a9002735b
Fernando Cavalcante <di39107@brscan.com.br>
2021-04-16 18:00:44 -0300

    84 - migrate javascript to a class called Sorter

---
+++ src/Sorter.ts
@@ -0,0 +1,18 @@
+export class Sorter {
+  constructor(public collection: /*TODO: fix me! */) {}
+
+  sorter(): void {
+    // const length = this.collection.length;
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,3 @@
-class Sorter {
-  constructor(public collection: number[]) {}
-
-  sorter(): void {
-    // const length = this.collection.length;
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
-        }
-      }
-    }
-  }
-}
-
 const sorter = new Sorter([10, 3, -5, 0]);
 sorter.sorter();
 console.log(sorter.collection);
